41208ee2f89a1d842f99eaa509ba79222cdadb14
Bryan Delianov <bryan.delianov@gmail.com>
2025-06-30 11:53:43 +0200

    first commit

---
+++ database/seeders/Cp_villeSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Cp_villeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
